7d17e5c84d93a0684d26c7797e6720a7ef78922b
Christopher Fahlin <chris_fahlin@comcast.net>
2022-07-31 20:54:51 -0700

    Added a more advanced sample, from the new vue generator

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,19 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
 module.exports = {
-    env: {
-      node: true,
+  root: true,
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript/recommended",
+    "@vue/eslint-config-prettier",
+  ],
+  overrides: [
+    {
+      files: ["cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}"],
+      extends: ["plugin:cypress/recommended"],
     },
-    extends: [
-        'eslint:recommended',
-        "plugin:vue/vue3-recommended",
-        "prettier"
-      ],
-    rules: {
-      // override/add rules settings here, such as:
-      // 'vue/no-unused-vars': 'error'
-    }
-  }
+  ],
+  ignorePatterns: ["src-tauri/**/*"],
+};
